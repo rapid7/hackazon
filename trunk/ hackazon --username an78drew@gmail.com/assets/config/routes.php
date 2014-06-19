c989eb302299e2983a4bc12f45230efced0b55ec
dpodgurskiy@ntobjectives.com <dpodgurskiy@ntobjectives.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-06-19 12:16:52 +0000

    removed wrong commit

--- trunk/ hackazon --username an78drew@gmail.com/assets/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'default' => array('(/<controller>(/<action>(/<id>)))', 
-                               array(
-                                    'controller' => 'home',
-                                    'action' => 'index'
-				     )
-				),
-);
