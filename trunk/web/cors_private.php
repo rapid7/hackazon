67c4db9e8ecf612063888329c0274aac38c3b4ad
dpodgurskiy@ntobjectives.com <dpodgurskiy@ntobjectives.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-07-03 08:44:22 +0000

    test files - to be removed

---
+++ trunk/web/cors_private.php
@@ -0,0 +1,16 @@
+<?  header('Access-Control-Allow-Origin: http//www.webscantest.com'); ?>
+
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<TITLE>Private CORS</TITLE>
+
+</HEAD>
+<BODY onload="ajaxCors()">
+
+<h3>CORS private page </h3>
+It can be accessed only with orign = webscantest.com or from page with the same domain
+<h3>Some interesting text here</h3>
+
+</BODY>
+</HTML>
---
+++ trunk/web/cors_public.php
@@ -0,0 +1,15 @@
+<?  header('Access-Control-Allow-Origin: *'); ?>
+
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<TITLE>Public CORS</TITLE>
+
+</HEAD>
+<BODY>
+<h3>CORS public page </h3>
+It can be accessed with any orign
+<h3>Some interesting text here</h3>
+</p>
+</BODY>
+</HTML>
