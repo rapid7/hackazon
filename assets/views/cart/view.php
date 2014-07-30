f4ebe75f74c2b88f21de12e540a25aabd9977909
alisovtsev <alisovtsev@gmail.com>
2014-07-30 17:20:24 +0000

    added new markup

--- assets/views/cart/view.php
+++ assets/views/cart/view.php
@@ -19,6 +19,12 @@
             }
         });
     }
+    $(function () {
+        $(".plus_btn").click(function() {
+            //console.warn($(this).parents('tr').children('input'));TODO
+        });
+    });
+
 </script>
 <div class="container">
 
@@ -30,17 +36,13 @@
                 </li>
                 <li class="active">My Cart</li>
             </ol>
-            <table class="table table-striped">
-                <tr>
-                    <th></th><th>Name</th><th>Price</th><th>Quantity</th>
-                </tr>
 
             <?php
-            foreach ($items as $item):?>
+            /*foreach ($items as $item):?>
                 <?php
                 $product = $item->getProduct();
                 ?>
-                <tr id="tr_item_<?=$item->id?>">
+                <tr id="tr_item_<?php echo $item->id?>">
                     <td>
                         <img class="img-responsive img-home-portfolio" style="width:100px" src="/products_pictures/<?=$product['picture']?>" alt="photo <?=$product['name']?>">
                     </td>
@@ -63,18 +65,383 @@
                         </select></h4>
                     </td>
                 </tr>
-            <?php endforeach;?>
-            </table>
+            <?php endforeach;*/?>
         </div>
 
-        <div class="col-md-3 col-sm-4 sidebar">
-            Subtotal (<span id="items_qty"><?=$itemQty?></span> items): $<span id="total_price"><?=$totalPrice?></span>
-
-            <a class="btn btn-primary" id="add_to_cart" onclick="alert('Coming Soon');return false;" href="#">Proceed to checkout</a>
-        </div>
 
     </div>
     <!-- /.row -->
+
+    <div class="row">
+    <div class="col-xs-12">
+
+    <!-- START CONTENT ITEM -->
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#step1" data-toggle="tab"><b>step 1:</b> overview</a></li>
+        <li class=""><a href="#step2" data-toggle="tab"><b>step 2:</b> personal info</a></li>
+        <li><a href="#step3" data-toggle="tab"><b>step 3:</b> confirmation</a></li>
+        <li><a href="#step4" data-toggle="tab"><b>step 4:</b> payment</a></li>
+    </ul>
+
+    <div class="tab-content">
+
+    <div class="tab-pane active" id="step1">
+        <div class="row">
+            <div class="col-xs-12">
+                <table class="table table-bordered table-order">
+                    <thead>
+                    <tr>
+                        <th class="hidden-xs">&nbsp;</th>
+                        <th></th>
+                        <th>count</th>
+                        <th>each (incl. tax)</th>
+                        <th>total</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($items as $item):?>
+                    <?php
+                    $product = $item->getProduct();
+                    ?>
+                    <tr id="tr_item_<?php echo $item->id?>">
+                        <td class="hidden-xs"><img class="img-responsive img-home-portfolio" style="width:100px" src="/products_pictures/<?=$product['picture']?>" alt="photo <?=$product['name']?>"></td>
+                        <td>
+                            <h4><a href="/product/view/<?=$product['productID']?>"><?=$product['name']?></a></h4>
+                        </td>
+                        <td>
+
+                            <div class="input-group">
+														<span class="input-group-btn">
+															<button class="btn btn-default minus_btn" type="button">
+																<span class="glyphicon glyphicon-minus">
+															</span></button>
+														</span>
+                                <input type="text" onchange="update_qty(<?=$item->id?>, this.value)" class="form-control" value="<?php echo $item->qty?>">
+														<span class="input-group-btn">
+															<button class="btn btn-default plus_btn" type="button">
+																<span class="glyphicon glyphicon-plus">
+															</span></button>
+														</span>
+                            </div>
+
+                        </td>
+                        <td align="right">
+                            <span>$ <?php echo $item->price?>,- </span>
+                        </td>
+                        <td align="right">
+                            <span>$ <?php echo $item->price*$item->qty?>,- </span>
+                        </td>
+                    </tr>
+                    <?php endforeach;?>
+                    <tr>
+                        <td align="right" colspan="3">
+                            <form class="form-horizontal">
+                                <fieldset>
+                                    <div class="form-group">
+                                        <label class="col-xs-4 control-label">shipping: </label>
+                                        <div class="col-xs-8">
+                                            <select class="form-control">
+                                                <option>mail</option>
+                                                <option>collect</option>
+                                                <option>express</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </td>
+                        <td align="right" colspan="2">
+                            <span class="label label-success">free</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right" colspan="3">
+                            <form class="form-horizontal">
+                                <fieldset>
+                                    <div class="form-group">
+                                        <label class="col-xs-4 control-label">payment: </label>
+                                        <div class="col-xs-8">
+                                            <select class="form-control">
+                                                <option>wire transfer</option>
+                                                <option>paypal</option>
+                                                <option>creditcard</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </td>
+                        <td align="right" colspan="2">
+                            <span class="label label-success">free</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right" colspan="3">Quantity items:<br>total: </td>
+                        <td align="right" colspan="2">
+                            $ <span id="items_qty"><?=$itemQty?></span>,-<br>
+                            $ <span id="total_price"><?=$totalPrice?>,-<br>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-4">
+                <a href="products.html" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span> Continue shopping</a>
+            </div>
+            <div class="col-xs-4">
+                <button class="btn btn-default"><span class="glyphicon glyphicon-trash"></span> Empty cart</button>
+            </div>
+            <div class="col-xs-4">
+                <button class="btn btn-primary pull-right" data-target="#step2" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right"></span></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane" id="step2">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <form class="form-horizontal well">
+                    <fieldset>
+                        <legend>Personal info</legend>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companyname">Company name</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companyname" type="text" placeholder="Companyname">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companytradingregister">Register</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companytradingregister" type="text" placeholder="Register">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companytaxregister">Tax number</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companytaxregister" type="text" placeholder="Tax number">
+                                <p class="help-block">Tax number required</p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companycontact">Contactperson</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companycontact" type="text" placeholder="Contactperson">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companyemail">E-mail address</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companyemail" type="text" placeholder="naam@domein.nl">
+                            </div>
+                        </div>
+                        <br>
+                        <legend>Invoice address</legend>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companyaddress">Address</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companyaddress" type="text" placeholder="Adres">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companyzip">Zipcode</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companyzip" type="text" placeholder="Zipcode">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companycity">City</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companycity" type="text" placeholder="City">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companycountry">Country</label>
+                            <div class="col-xs-8">
+                                <select class="form-control" id="companycountry">
+                                    <option>Country 01</option>
+                                    <option>Country 02</option>
+                                </select>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <legend>Postal address</legend>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companypostaladdress">Address</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companypostaladdress" type="text" placeholder="Address">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companypostalzip">Zipcode</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companypostalzip" type="text" placeholder="Zipcode">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companypostalcity">City</label>
+                            <div class="col-xs-8">
+                                <input class="form-control" id="companypostalcity" type="text" placeholder="City">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-xs-4 control-label" for="companypostalcountry">Country</label>
+                            <div class="col-xs-8">
+                                <select class="form-control" id="companypostalcountry">
+                                    <option>Country 01</option>
+                                    <option>Country 02</option>
+                                </select>
+                            </div>
+                        </div>
+                        <br>
+                        <legend>Comments</legend>
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                <textarea class="form-control" id="comments" rows="12" maxlength="255"></textarea>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" value="yes" id="termsandconditions">Yes, I agree to the <a href="#">terms &amp; conditions</a>
+                            </label>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-6">
+                <button class="btn btn-default" data-target="#step1" data-toggle="tab"><span class="glyphicon glyphicon-chevron-left"></span> Overview</button>
+            </div>
+            <div class="col-xs-6">
+                <button class="btn btn-primary pull-right" data-target="#step3" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right icon-white"></span></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="tab-pane" id="step3">
+        <div class="row">
+
+            <div class="col-xs-12 col-sm-5">
+
+                <div class="well">
+
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Peronsal info</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                company name<br>
+                                contactperson name<br>
+                                register: register number<br>
+                                tax number: tax number<br>
+                                <br>
+                                <b>invoice address</b><br>
+                                address 123<br>
+                                zipcode city nl<br>
+                                email@example.com<br>
+                                <br>
+                                <b>Comments</b>
+                                <br>
+                                <i>Comments are here</i>
+                            </td>
+                        </tr>
+                        </thead>
+                    </table>
+
+                </div>
+
+            </div>
+
+            <div class="col-xs-12 col-sm-7">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Overview</th>
+                        <th width="50">count</th>
+                        <th width="70">total</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Product A</td>
+                        <td align="center">1</td>
+                        <td align="right">€ 123,-</td>
+                    </tr>
+                    <tr>
+                        <td>Product B</td>
+                        <td align="center">2</td>
+                        <td align="right">€ 2,-</td>
+                    </tr>
+                    <tr>
+                        <td>Shipping: express</td>
+                        <td align="right" colspan="2">€ 0,-</td>
+                    </tr>
+                    <tr>
+                        <td>Payment: wiretransfer</td>
+                        <td align="right" colspan="2">€ 0,-</td>
+                    </tr>
+                    <tr>
+                        <td align="right" colspan="3"><strong>€ 125,-</strong></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-xs-6">
+                <button class="btn btn-default" data-target="#step2" data-toggle="tab"><span class="glyphicon glyphicon-chevron-left"></span> personal info</button>
+            </div>
+            <div class="col-xs-6">
+                <button class="btn btn-primary pull-right" data-target="#step4" data-toggle="tab">Next <span class="glyphicon glyphicon-chevron-right"></span></button>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="tab-pane" id="step4">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <h2>Payment success</h2>
+                <p>Your order will be processed as soon as possible.</p>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <h2>Delivery</h2>
+                <p>With express shipping, your order will arrive within 24 hours.</p>
+            </div>
+        </div>
+        <br><br><br>
+        <div class="row">
+            <div class="col-xs-6">
+            </div>
+            <div class="col-xs-6">
+                <a href="index.html" class="btn btn-primary pull-right">Go to home <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    <br>
+
+    </div>
+    <!-- END CONTENT ITEM -->
+
+    </div>
+
 
 </div>
 <!-- /.container -->
