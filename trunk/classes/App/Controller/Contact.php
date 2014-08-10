3775112f3a7349179d50dd5cd7d2ffc94ded082e
alisovtsev@gmail.com <alisovtsev@gmail.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-08-10 14:04:33 +0000

    post contact us messages with validation fields

--- trunk/classes/App/Controller/Contact.php
+++ trunk/classes/App/Controller/Contact.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 class Contact extends \App\Page {
 
 	public function action_index(){
+        if ($this->request->is_ajax()) {
+            $post = $this->request->post();
+            $this->pixie->orm->get('ContactMessages')->create($post);
+            $this->execute = false;
+        }
 		$this->view->subview = 'contact';
 	}
 	
