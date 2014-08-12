29635c85705f20f12580bd99cc6b6042729379fe
nick.chervyakov@gmail.com <nick.chervyakov@gmail.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-08-12 10:24:29 +0000

    Fix demo data

--- trunk/database/migrations/2014.08.10_options.sql
+++ trunk/database/migrations/2014.08.10_options.sql
@@ -5,6 +5,6 @@ ALTER TABLE `tbl_products_opt_val_variants` COMMENT='' ENGINE='InnoDB';
 ALTER TABLE `tbl_product_options_values` CHANGE COLUMN `VariantID` `ID` INT(11) NOT NULL; 
 ALTER TABLE `tbl_product_options_values` ADD PRIMARY KEY (`ID`);
 
-ALTER TABLE `tbl_product_options_val` CHANGE COLUMN `OptionID` `variantID` INT(11) NOT NULL;
+ALTER TABLE `tbl_product_options_values` CHANGE COLUMN `OptionID` `variantID` INT(11) NOT NULL;
 
 ALTER TABLE `tbl_product_options_values` ADD INDEX (`productID`);
