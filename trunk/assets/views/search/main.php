344f030127fb2dc026aae73af903607ec357e9b8
dpodgurskiy@ntobjectives.com <dpodgurskiy@ntobjectives.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-08-20 12:12:00 +0000

    Small changes/removed unused files

--- trunk/assets/views/search/main.php
+++ trunk/assets/views/search/main.php
@@ -1,6 +1,6 @@
 <div class="section">
     <div class="container">
         <?php
-        include($common_path . "search_product_items.php"); ?>
+        include("search_product_items.php"); ?>
     </div>
 </div>
