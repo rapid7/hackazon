72313ae92cfdfd5b1e9fd2e40704c980b8b645ca
dpodgurskiy <dpodgurskiy@ntobjectives.com>
2014-08-28 11:32:05 +0000

    Small fixes

--- assets/views/account/document.php
+++ assets/views/account/document.php
@@ -5,7 +5,7 @@
             <h1 class="page-header"><?php $_($pageTitle); ?></h1>
             <ol class="breadcrumb">
                 <li><a href="/">Home</a></li>
-                <li><a href="/page">Documents</a></li>
+                <li><a href="/account/documents">Documents</a></li>
                 <li class="active"><?php $_($pageTitle); ?></li>
             </ol>
         </div>
