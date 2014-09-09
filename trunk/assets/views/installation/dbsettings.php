09098c8c26020e822d310b7bf0727b7afce8a0b3
nick.chervyakov@gmail.com <nick.chervyakov@gmail.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-09-09 07:10:09 +0000

    Field Labels for Installation Wizard

--- trunk/assets/views/installation/dbsettings.php
+++ trunk/assets/views/installation/dbsettings.php
@@ -1,17 +1,21 @@
 <form action="/install/db_settings" id="dbSettingsForm" method="POST">
     <div class="form-group">
+        <label>Host:</label>
         <input class="form-control" type="text" name="host" value="<?php $_(isset($host) ? $host : ''); ?>" placeholder="Host" required />
     </div>
 
     <div class="form-group">
-        <input class="form-control" type="text" name="user" value="<?php $_(isset($user) ? $user : ''); ?>" placeholder="Username" required />
+        <label>User:</label>
+        <input class="form-control" type="text" name="user" value="<?php $_(isset($user) ? $user : ''); ?>" placeholder="User" required />
     </div>
 
     <div class="form-group">
+        <label>Password:</label>
         <input class="form-control" type="password" name="password" value="<?php $_(isset($password) ? $password : ''); ?>" placeholder="Password" />
     </div>
 
     <div class="form-group">
+        <label>Database Name:</label>
         <input class="form-control" type="text" name="db" value="<?php $_(isset($db) ? $db : ''); ?>" placeholder="Database" required />
     </div>
 
