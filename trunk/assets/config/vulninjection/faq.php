e1c2634d075ff2302f1981e0c7aa036cfc5ff69b
dpodgurskiy@ntobjectives.com <dpodgurskiy@ntobjectives.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-09-19 08:56:08 +0000

    Fixed faq vuln config

--- trunk/assets/config/vulninjection/faq.php
+++ trunk/assets/config/vulninjection/faq.php
@@ -1,12 +1,10 @@
 <?php
 return [
     'fields' => [
-        'q' => ['sql'],
         'userQuestion' => [
             'xss',
             'db_field' => 'faq.question'
         ],
-        
         'userEmail' => [
             'db_field' => 'faq.email'
         ]
