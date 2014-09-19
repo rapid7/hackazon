31a3c8721758d3f6edbc67a88172822ddcaa8c46
dpodgurskiy <dpodgurskiy@ntobjectives.com>
2014-09-19 07:17:43 +0000

    Fixed small issues

---
+++ assets/views/user/terms.php
@@ -0,0 +1,72 @@
+<!-- Page Content -->
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-lg-12">
+        </div>
+    </div>
+    
+    <div class="row" >
+        <h2>Terms and Conditions <small>registration rules</small></h2>
+        <ol class="breadcrumb">
+            <li><a href="/">Home</a>
+            </li>
+            <li class="active">Terms and Conditions of registration</li>
+        </ol>
+	<div class="sliceTop"></div>
+	
+	<p>Welcome to the registration to Hackazon ("us", "we") website and its related services. You may access many areas of our website without registering your details with us.  Certain areas are only open to you if you register.  The terms and conditions which govern material submitted by you to us and your use of the Hackazon website (including all 'Hackazon' branded microsites and any email bulletins) are governed by our general "Terms and Conditions of Reading".  These Terms and Conditions of Registration are in addition to the general Terms and Conditions of Reading. </p>  
+	<ul>
+		<li><span class="index">1.</span> Registration
+			<ul>
+				<li><span class="index">1.1</span> By registering here and creating your "Hackazon Profile", you can access different services that are offered by us without having to register for each service separately. If a service you wish to subscribe to has additional terms and conditions, you will be asked to accept these separately. </li> 
+				<li><span class="index">1.2</span> You agree that:
+					<ul>
+						<li>
+							<span class="index">(1)</span> you will keep your username and password safe, and won't share them with anyone.
+						</li>
+						<li>
+							<span class="index">(2)</span> you will not pass yourself off as someone else or create multiple, false accounts.
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li><span class="index">2.</span> Term
+			<ul>
+				<li><span class="index">2.1</span>	If you breach these or any of our other terms and conditions we reserve the right to close your account, if we do so, we may close all accounts you have open in your name.</li>
+				<li><span class="index">2.2</span>	To deactivate your account please contact our customer services department; their details can be found under "contact us" on your Hackazon Profile page. </li>
+			</ul>
+		</li> 
+		<li><span class="index">3.</span> Administration
+			<ul>
+				<li>
+					<span class="index">3.1</span> You can update your personal details (including your account and marketing preferences) by accessing your account at your Hackazon Profile page and making any necessary changes; this will update your details across all services that you have subscribed to. 
+				</li>
+			</ul>
+		</li> 
+
+		<li><span class="index">4.</span>	General
+			<ul>
+				<li>
+					<span class="index">4.1</span>	No waiver by us of any breach of these terms shall constitute a waiver of any other prior or subsequent breach and we shall not be affected by any delay, failure or omission to enforce or express forbearance granted in respect of any of your obligations.
+				</li> 
+				<li>
+					<span class="index">4.2</span>	The rights and remedies of is under these terms are independent, cumulative and without prejudice to its rights under the law.
+				</li> 
+				<li>
+					<span class="index">4.3</span>	These terms are not intended to create and shall not create any rights, entitlements, claims or benefits enforceable by any third party by virtue of the Contracts (Rights of Third Parties) Act 1999.
+				</li> 
+				<li>
+					<span class="index">4.4</span>	These terms and/or your use of the website shall be governed by and construed in accordance with English law and the English Courts shall have exclusive jurisdiction over any dispute which may arise.
+				</li>
+			</ul>
+		</li>
+	</ul>
+	<div class="sliceBottom"></div>
+
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container -->
