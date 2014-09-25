aff6f6cb25f5cb966b0345003ae1dee7f8a5ce3b
dpodgurskiy <dpodgurskiy@ntobjectives.com>
2014-09-25 08:28:32 +0000

    Added doc with links to REST service

---
+++ assets/views/content_pages/help_articles/rest.php
@@ -0,0 +1,9 @@
+<p>
+You can use our REST service to get actual information about your orders.
+</p>
+<p>
+Orders - <a href="http://webscantest.new/api/order">http://webscantest.new/api/order</a> or <a href="http://webscantest.new/api/order?_format=xml">http://webscantest.new/api/order?_format=xml</a>
+</p>
+<p>
+Address - <a href="http://webscantest.new/api/OrderAddresses">http://webscantest.new/api/orderaddresses</a> or <a href="http://webscantest.new/api/orderaddresses?_format=xml">http://webscantest.new/api/orderaddresses?_format=xml</a>
+</p>
