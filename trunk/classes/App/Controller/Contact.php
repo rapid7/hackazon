19bd58c7f4f4cce31700ecffb2fbf2b56de4a609
dk@ntobjectives.com <dk@ntobjectives.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2014-11-20 21:01:47 +0000

    Trying to make the site vulnerable again

--- trunk/classes/App/Controller/Contact.php
+++ trunk/classes/App/Controller/Contact.php
@@ -7,16 +7,17 @@ class Contact extends \App\Page {
     public function action_index() {
         $this->view->pageTitle = "Contact us";
         if ($this->request->method == 'POST') {
-            $this->checkCsrfToken('contact');
+					$this->checkCsrfToken('contact');
             //$post = json_decode($this->request->post()['data']);
-            $post = json_decode($this->request->rawRequestData(), true);
+            $post = json_decode($this->request->post()['data']);
             $this->pixie->orm->get('ContactMessages')->create($post);
-            if ($this->request->is_ajax()) {
+						if ($this->request->is_ajax()) {
                 //$this->jsonResponse(null);
-                $this->execute = false;
-            }
+								$this->execute = false;
+						}
         }
         $this->view->subview = 'pages/contact';
     }
+
 }
 
