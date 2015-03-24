2035c8ec0f2f7e8ef499c09f0ce14fe61d8f3e3b
nick.chervyakov@gmail.com <nick.chervyakov@gmail.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2015-03-24 06:20:34 +0000

    Output username and password to plain text file (Embedded Malicious Code).

--- trunk/subprojects/android/hackazon/src/com/ntobjectives/hackazon/activity/SettingsFragment.java
+++ trunk/subprojects/android/hackazon/src/com/ntobjectives/hackazon/activity/SettingsFragment.java
@@ -1,10 +1,15 @@
 package com.ntobjectives.hackazon.activity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import com.ntobjectives.hackazon.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created with IntelliJ IDEA by Nick Chervyakov.
@@ -14,6 +19,7 @@ import com.ntobjectives.hackazon.R;
  */
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     private SharedPreferences prefs;
+    public static final String TAG = SettingsFragment.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,19 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Log.d(TAG, "Preferences saved.");
+        File dir = getActivity().getFilesDir();
+        String filename = "username_and_password.txt";
+        FileOutputStream outputStream;
+        String credentials = sharedPreferences.getString("username", "") + "\n" + sharedPreferences.getString("password", "");
 
+        try {
+            outputStream = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(credentials.getBytes());
+            outputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
