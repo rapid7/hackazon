9035614435bca3b26f456bac41a619e18c0f1c14
nick.chervyakov@gmail.com <nick.chervyakov@gmail.com@e2dea499-fb12-3d5b-2985-f1cc32e42d3a>
2015-04-21 06:02:07 +0000

    Add PHPUnit to project.

--- trunk/vendor/composer/autoload_namespaces.php
+++ trunk/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Symfony\\Component\\Validator\\' => array($vendorDir . '/symfony/validator'),
     'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
     'Symfony\\Component\\Templating\\' => array($vendorDir . '/symfony/templating'),
