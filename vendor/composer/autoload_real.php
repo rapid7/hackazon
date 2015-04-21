68e6b87fd824946831fa46e81907d39f9e07a931
nick.chervyakov <nick.chervyakov@gmail.com>
2015-04-21 06:02:07 +0000

    Add PHPUnit to project.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInitf63e49df0504d6d846a10a392eb17251
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf63e49df0504d6d846a10a392eb17251', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
